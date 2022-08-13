a60bb42275acc959a765564641b8b4dd8268fab3
GEUNA <geuna0204@gmail.com>
2022-08-13 12:24:52 +0900

    [add] add provider in index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from 'App';
+import GlobalStyle from 'styles/GlobalStyle';
+import { Provider } from 'react-redux';
+import store from 'redux/configStore';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <GlobalStyle />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
