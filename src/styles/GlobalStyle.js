27bf066cdb8ab04670eff1599c46a5f21c988528
GEUNA <geuna0204@gmail.com>
2022-08-13 12:23:25 +0900

    [add] style initialize

---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,58 @@
+import { createGlobalStyle } from 'styled-components';
+import { colors } from 'styles/theme';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    font-size: 18px;
+    box-sizing: border-box;
+    font-family: 'Noto Sans KR', sans-serif;
+  }
+  
+  body {
+    background-color: ${colors.white};
+    margin: 0 auto;
+  }
+
+  h1 {
+    margin: 0;
+    font-size: 28px;
+    font-weight: 500;
+  }
+
+  h2 {
+    margin: 0;
+    font-size: 32px;
+  }
+
+  h3 {
+    margin: 0;
+    font-size: 20px;
+  }
+
+  ul {
+    list-style: none;
+  }
+
+  p {
+    font-size: 18px;
+    margin: 0;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  button, input, textarea {
+    :focus {
+      outline: none;
+    }
+    :hover {
+    }
+  }
+`;
+
+export default GlobalStyle;
