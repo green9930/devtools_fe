a4c34f6f48d6f38a89722dc82a49514036fd998f
GEUNA <geuna0204@gmail.com>
2022-08-15 10:15:24 +0900

    [add] add react-cookie

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import App from 'App';
 import GlobalStyle from 'styles/GlobalStyle';
 import { Provider } from 'react-redux';
 import store from 'redux/configStore';
+import { CookiesProvider } from 'react-cookie';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <GlobalStyle />
     <Provider store={store}>
-      <App />
+      <CookiesProvider>
+        <App />
+      </CookiesProvider>
     </Provider>
   </React.StrictMode>
 );
