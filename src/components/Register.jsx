78d22f36b3f8de66b22e6a18551e007ad6c42c59
GEUNA <geuna0204@gmail.com>
2022-08-16 03:29:23 +0900

    [add] add register validation

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,23 +1,29 @@
 import { useState } from 'react';
-import styled from 'styled-components';
 import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
 import Btn from './elements/Btn';
 import Input from './elements/Input';
-import { BsFillEyeFill, BsFillEyeSlashFill } from 'react-icons/bs';
 import regExpVali from 'utils/regExpVali';
+import { colors } from 'styles/theme';
+import axios from 'axios';
+import { BsFillEyeFill, BsFillEyeSlashFill } from 'react-icons/bs';
 
 const Register = () => {
+  /* TEST DATA ---------------------------------------------------------------- */
+  const test = {
+    username: 'aaaaa',
+  };
+
   const navigate = useNavigate();
-  const [isVisible, setIsVisible] = useState(false);
+
+  /* USER 객체 ------------------------------------------------------------------ */
   const [user, setUser] = useState({
     username: '',
     password: '',
   });
-  const [userId, setUserId] = useState('');
-  const [password, setPassword] = useState('');
+  /* CONFIRM PASSWORD --------------------------------------------------------- */
   const [confirmPassword, setConfirmPassword] = useState('');
-  const [checkDuplicate, setCheckDuplicate] = useState(false);
-  const [isPassed, setIsPassed] = useState(true);
+  /* VALIDATION MESSAGE ------------------------------------------------------- */
   const [usernameMessage, setUsernameMessage] = useState(
     '아이디를 입력해 주세요.(영문 또는 숫자 4~12자)'
   );
@@ -30,56 +36,108 @@ const Register = () => {
     passed: '사용 가능한 아이디입니다.',
     notPassed: '이미 사용 중인 아이디입니다.',
   };
-
+  /* REGISTER FORM 통과 확인 ------------------------------------------------------ */
   const [isUsernameAvailable, setIsUsernameAvailable] = useState(false);
   const [isPasswordAvailable, setIsPasswordAvailable] = useState(false);
   const [isPasswordConfirmed, setIsPasswordConfirmed] = useState(false);
+  /* 중복 검사 활성화 ---------------------------------------------------------------- */
+  const [isUsernameActive, setIsUsernameActive] = useState(false);
+  /* 중복 검사 통과 확인 -------------------------------------------------------------- */
+  const [checkDuplicate, setCheckDuplicate] = useState(false);
+  /* 입력한 비밀번호 보기 -------------------------------------------------------------- */
+  const [isVisible, setIsVisible] = useState(false);
 
-  const handleSubmit = (e) => {};
-
-  // const handleChange = (e) => {
-  //   const target = e.target.value;
-  //   const name = e.target.name;
-
-  //   const result = regExpVali(target, name);
-  //   console.log(result);
-
-  //   if (name === 'username' || 'password') {
-  //     setUser({ ...user, [name]: target })
-  //     if (regExpVali(target, name)) {
-
-  //     }
-  //   };
-  //   if (name === 'confirmPassword') setConfirmPassword(target);
-
-  //   target === password
-  //     ? setConfirmPasswordMessage('비밀번호가 일치합니다.')
-  //     : setConfirmPasswordMessage('비밀번호가 일치하지 않습니다.');
-  // };
-
-  const handleChangeUsername = (e) => {
-    const { value, name } = e.target;
-
-    setUser({ ...user, username: value });
-    if (regExpVali(value, name)) {
-      setUsernameMessage('올바른 아이디 형식입니다.');
-      setIsUsernameAvailable(true);
+  /* 회원가입 --------------------------------------------------------------------- */
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log('isUsernameAvailable', isUsernameAvailable);
+    console.log('isPasswordAvailable', isPasswordAvailable);
+    console.log('isPasswordConfirmed', isPasswordConfirmed);
+    try {
+      console.log('USER', user);
+      // const res = await axios.post(`http://3.34.185.48/api/signup`, user);
+      // console.log(res);
+      navigate('/login');
+    } catch (error) {
+      console.error(error);
     }
   };
 
-  const handleChangePassword = (e) => {
-    const { value, name } = e.target;
+  /* 아이디 유효성 검사 --------------------------------------------------------------- */
+  const handleChangeUsername = (e) => {
+    const username = e.target.value.substr(0, 12);
+    const name = e.target.name;
 
-    setUser({ ...user, password: value });
-    if (regExpVali(value, name)) {
-      setUsernameMessage('올바른 비밀번호 형식입니다.');
+    setIsUsernameAvailable(false);
+    setCheckDuplicate(false);
+    setUser({ ...user, username: username });
+
+    if (regExpVali(username, name)) {
+      setUsernameMessage('올바른 아이디 형식입니다.');
+      setIsUsernameActive(true);
+    } else {
+      setUsernameMessage('아이디를 입력해 주세요.(영문 또는 숫자 4~12자)');
+      setIsUsernameActive(false);
+    }
+  };
+
+  /* 아이디 중복 검사 ---------------------------------------------------------------- */
+  const handleCheckDuplicate = async () => {
+    // const res = await axios.post(`http://3.34.185.48/api/register/username`, {
+    //   username: user.username,
+    // });
+
+    if (user.username === test.username) {
+      setUsernameMessage('이미 사용 중인 아이디입니다.');
+      setIsUsernameAvailable(false);
+      setCheckDuplicate(false);
+    } else {
+      setUsernameMessage('사용 가능한 아이디입니다');
+      setIsUsernameAvailable(true);
+      setCheckDuplicate(true);
+      setIsUsernameActive(false);
+    }
+  };
+
+  /* 비밀번호 유효성 검사 -------------------------------------------------------------- */
+  const handleChangePassword = (e) => {
+    const password = e.target.value.substr(0, 16).trim();
+    const name = e.target.name;
+
+    setConfirmPassword('');
+    setConfirmPasswordMessage('비밀번호를 확인해 주세요.');
+    setIsPasswordConfirmed(false);
+
+    setUser({ ...user, password: password });
+
+    if (regExpVali(password, name)) {
+      setPasswordMessage('올바른 비밀번호 형식입니다.');
       setIsPasswordAvailable(true);
+    } else {
+      setPasswordMessage(
+        '비밀번호를 입력해 주세요.(영문, 숫자, 특수문자 포함 8~16자)'
+      );
+      setIsPasswordAvailable(false);
+    }
+  };
+
+  /* 비밀번호 확인 유효성 검사 ----------------------------------------------------------- */
+  const handleChangeConfirmPassword = (e) => {
+    const confirmPw = e.target.value.substr(0, 16);
+
+    setConfirmPassword(confirmPw);
+    if (confirmPw === user.password) {
+      setConfirmPasswordMessage('비밀번호가 일치합니다.');
+      setIsPasswordConfirmed(true);
+    } else {
+      setConfirmPasswordMessage('비밀번호를 확인해 주세요.');
+      setIsPasswordConfirmed(false);
     }
   };
 
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <InputContainer>
           <div>
             <IdContainer>
@@ -88,7 +146,7 @@ const Register = () => {
                 id="userId"
                 size="medium"
                 name="username"
-                placeHolderText="ID를 입력해 주세요."
+                placeHolderText="ID"
                 value={user.username}
                 onChangeHandler={handleChangeUsername}
               />
@@ -98,16 +156,21 @@ const Register = () => {
               >
                 <Btn
                   size="small"
-                  variant="green_outline"
-                  isAble={isPassed ? true : false}
-                  onClickHandler={() => setCheckDuplicate(!checkDuplicate)}
+                  variant={isUsernameActive ? 'green_outline' : 'disabled'}
+                  isAble={isUsernameActive ? false : true}
+                  onClickHandler={handleCheckDuplicate}
                 >
                   중복 확인
                 </Btn>
               </StButton>
             </IdContainer>
             <AlertMessageContainer>
-              <span>{usernameMessage}</span>
+              <StyledNameMessage
+                isAvailable={isUsernameActive}
+                checkDuplicate={checkDuplicate}
+              >
+                {usernameMessage}
+              </StyledNameMessage>
             </AlertMessageContainer>
           </div>
           <div>
@@ -118,7 +181,7 @@ const Register = () => {
                 size="medium"
                 type={isVisible ? 'text' : 'password'}
                 name="password"
-                placeHolderText="비밀번호를 입력해 주세요."
+                placeHolderText="Password"
                 value={user.password}
                 onChangeHandler={handleChangePassword}
               />
@@ -127,7 +190,9 @@ const Register = () => {
               </span>
             </PasswordContainer>
             <AlertMessageContainer>
-              <span>{passwordMessage}</span>
+              <StyledMessage isAvailable={isPasswordAvailable}>
+                {passwordMessage}
+              </StyledMessage>
             </AlertMessageContainer>
           </div>
           <ConfirmPasswordContainer>
@@ -137,17 +202,28 @@ const Register = () => {
               size="medium"
               type="password"
               name="confirmPassword"
-              placeHolderText="비밀번호를 확인해 주세요."
+              placeHolderText="Confirm Password"
               value={confirmPassword}
-              // onChangeHandler={handleChange}
+              onChangeHandler={handleChangeConfirmPassword}
             />
             <AlertMessageContainer>
-              <span>{confirmPasswordMessage}</span>
+              <StyledMessage isAvailable={isPasswordConfirmed}>
+                {confirmPasswordMessage}
+              </StyledMessage>
             </AlertMessageContainer>
           </ConfirmPasswordContainer>
         </InputContainer>
         <ButtonContainer>
-          <Btn>회원가입</Btn>
+          <Btn
+            type="submit"
+            isAble={
+              isUsernameAvailable && isPasswordAvailable && isPasswordConfirmed
+                ? false
+                : true
+            }
+          >
+            회원가입
+          </Btn>
           <Btn
             size="large"
             variant="black_outline"
@@ -176,7 +252,7 @@ const IdContainer = styled.div`
 
 const StButton = styled.div`
   position: absolute;
-  top: 5px;
+  top: 3px;
   right: -90px;
 `;
 
@@ -194,9 +270,20 @@ const AlertMessageContainer = styled.div`
   height: 30px;
   margin-top: 3px;
   margin-left: 3px;
-  span {
-    font-size: 14px;
-  }
+`;
+
+const StyledNameMessage = styled.span`
+  font-size: 14px;
+  color: ${({ isAvailable, checkDuplicate }) => {
+    if (checkDuplicate && !isAvailable) return `${colors.green1}`;
+    else if (isAvailable || !isAvailable) return `${colors.yellow}`;
+  }};
+`;
+
+const StyledMessage = styled.span`
+  font-size: 14px;
+  color: ${({ isAvailable }) =>
+    isAvailable ? `${colors.green1}` : `${colors.yellow}`};
 `;
 
 const ConfirmPasswordContainer = styled.div``;
