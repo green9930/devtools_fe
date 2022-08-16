4e770a7aff05b128749e2b1b118f9e4d9ff734e7
hyeonuk-choo <108967896+hyeonuk-choo@users.noreply.github.com>
2022-08-16 23:59:29 +0900

    댓글 추가

--- src/components/CommentForm.jsx
+++ src/components/CommentForm.jsx
@@ -1,31 +1,45 @@
-import styled from 'styled-components';
-import Input from 'components/elements/Input';
-import Btn from 'components/elements/Btn';
-import { useState } from 'react';
-import { colors } from 'styles/theme';
-import lengthVali from 'utils/lengthVali';
+import styled from "styled-components";
+import Input from "components/elements/Input";
+import Btn from "components/elements/Btn";
+import { useState } from "react";
+import { colors } from "styles/theme";
+import lengthVali from "utils/lengthVali";
+import { useDispatch } from "react-redux";
+import { __postComments } from "redux/modules/commentsSlice";
+import { useParams } from "react-router-dom";
+import { __getDetail } from "redux/modules/devToolsSlice";
 
-const CommentForm = () => {
-  const [alertMessage, setAlertMessage] = useState('');
-  const [text, setText] = useState('');
+const CommentForm = ({ devtool }) => {
+  const [alertMessage, setAlertMessage] = useState("");
+  const [text, setText] = useState("");
 
-  const loginuser = 'admin';
-
+  const loginuser = "admin";
+  const dispatch = useDispatch();
+  const { id } = useParams();
   const handleSubmit = (e) => {
     e.preventDefault();
-    text.trim() === ''
-      ? setAlertMessage('댓글을 입력해 주세요. (최대 40자)')
-      : setAlertMessage('');
+    text.trim() === ""
+      ? setAlertMessage("댓글을 입력해 주세요. (최대 40자)")
+      : setAlertMessage("");
+
+    dispatch(
+      __postComments({
+        id: id,
+        comments: text,
+      })
+    );
+    dispatch(__getDetail());
   };
 
   const handleChange = (e) => {
     const { val, verify } = lengthVali(e.target.value, 40);
     verify
-      ? setAlertMessage('댓글은 40자까지 작성할 수 있습니다.')
-      : setAlertMessage('');
+      ? setAlertMessage("댓글은 40자까지 작성할 수 있습니다.")
+      : setAlertMessage("");
     setText(val);
   };
-
+  // console.log("text", text, "id", id);
+  // console.log(devtool);
   return (
     <>
       <CommentFormContainer>
@@ -42,7 +56,7 @@ const CommentForm = () => {
             onChangeHandler={handleChange}
             value={text}
           />
-          <Btn variant="green_outline" size="small">
+          <Btn variant="green_outline" size="small" type="submit">
             댓글 작성
           </Btn>
         </form>
