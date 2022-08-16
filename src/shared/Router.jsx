904695756cebb5c0a61d7eb845339b724fd8c7e3
GEUNA <geuna0204@gmail.com>
2022-08-17 00:13:40 +0900

    [add] 비회원 페이지 이동 방지 기능 구현

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,19 +1,45 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import DetailPage from 'pages/DetailPage';
 import FormPage from 'pages/FormPage';
 import LoginPage from 'pages/LoginPage';
 import MainPage from 'pages/MainPage';
 import RegisterPage from 'pages/RegisterPage';
+import { useEffect } from 'react';
+import { userActions } from 'redux/modules/userSlice';
+import { useDispatch, useSelector } from 'react-redux';
 
 const Router = () => {
+  const dispatch = useDispatch();
+
+  const { isLogin } = useSelector((state) => state.user);
+
+  useEffect(() => {
+    dispatch(userActions.getUser());
+  }, [dispatch]);
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/form" element={<FormPage />} />
-        <Route path="/detail/:id" element={<DetailPage />} />
+        <Route
+          path="/"
+          element={isLogin ? <MainPage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/register"
+          element={isLogin ? <Navigate to="/" /> : <RegisterPage />}
+        />
+        <Route
+          path="/login"
+          element={isLogin ? <Navigate to="/" /> : <LoginPage />}
+        />
+        <Route
+          path="/form"
+          element={isLogin ? <FormPage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/detail/:id"
+          element={isLogin ? <DetailPage /> : <Navigate to="/login" />}
+        />
       </Routes>
     </BrowserRouter>
   );
