2f30989541c7a965b3063017dc28b8ae4445578c
GEUNA <geuna0204@gmail.com>
2022-08-16 23:26:26 +0900

    [add] add cookie func

---
+++ src/shared/cookies.js
@@ -0,0 +1,7 @@
+import { Cookies } from 'react-cookie';
+
+const cookies = new Cookies();
+
+export const getCookie = (name) => cookies.get(name);
+export const setCookie = (name, value) => cookies.set(name, value);
+export const removeCookie = (name) => cookies.remove(name);
