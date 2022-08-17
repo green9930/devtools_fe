fbc0ba102fa437dcc267b3b939778ee90d579f4f
GEUNA <geuna0204@gmail.com>
2022-08-17 16:19:13 +0900

    [add] add ErrorPage, UnAuthPage

---
+++ src/components/Error.jsx
@@ -0,0 +1,51 @@
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+import { colors } from 'styles/theme';
+import Btn from './elements/Btn';
+
+const Error = () => {
+  const navigate = useNavigate();
+
+  return (
+    <ErrorContainer>
+      <h2>404 Page Not Found</h2>
+      <div>
+        <span>페이지를 찾을 수 없습니다.</span>
+        <Btn variant="green_filled" onClickHandler={() => navigate('/')}>
+          홈으로 이동
+        </Btn>
+      </div>
+    </ErrorContainer>
+  );
+};
+
+export default Error;
+
+const ErrorContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  margin-top: 200px;
+  gap: 40px;
+
+  h2 {
+    font-size: 32px;
+    font-weight: 700;
+    color: ${colors.red};
+  }
+
+  div {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 80px;
+
+    span {
+      font-size: 20px;
+      font-weight: 500;
+    }
+  }
+`;
---
+++ src/components/UnAuth.jsx
@@ -0,0 +1,49 @@
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+import { colors } from 'styles/theme';
+import Btn from 'components/elements/Btn';
+import devtools_title from 'assets/devtools_title.svg';
+
+const UnAuth = () => {
+  const navigate = useNavigate();
+
+  return (
+    <ErrorContainer>
+      <h2>이 페이지는 로그인 후에 이용하실 수 있습니다.</h2>
+      <div>
+        <div>
+          <img src={devtools_title} alt="logo" />
+        </div>
+        <Btn variant="blue_filled" onClickHandler={() => navigate('/')}>
+          로그인 페이지로 이동
+        </Btn>
+      </div>
+    </ErrorContainer>
+  );
+};
+
+export default UnAuth;
+
+const ErrorContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  margin-top: 200px;
+  gap: 100px;
+
+  h2 {
+    font-size: 32px;
+    font-weight: 700;
+    color: ${colors.green1};
+  }
+
+  div {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 40px;
+  }
+`;
